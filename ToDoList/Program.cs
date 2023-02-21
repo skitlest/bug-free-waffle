37ba10a3fe81da8f8d979febeb05511e212718b4
Skitals <trudov.stanislav@gmail.com>
2023-02-22 01:33:15 +0200

    init new projects

---
+++ ToDoList/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+Console.WriteLine("Salut, Lume!");
+
